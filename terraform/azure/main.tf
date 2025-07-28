01489209d23dab7c9611c7d10ae0747297e6cdac
carlosacosta-tinamica <119498316+carlosacosta-tinamica@users.noreply.github.com>
2025-07-28 14:17:05 +0200

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -2,7 +2,28 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "rg-ejemplo-carlosacosta"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-tf-demo"
   location = "West Europe"
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "vm-tf-demo"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  admin_password      = "P@ssw0rd123456!"  # Cambia esto en producción o usa secrets
+  network_interface_ids = []
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "osdisk-tf-demo"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-10"
+    sku       = "20h2-pro"
+    version   = "latest"
+  }
 }
