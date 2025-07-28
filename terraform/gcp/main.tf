7b4df25fab5d2dbfbd5d3dc91348ba2974fdeba1
carlosacosta-tinamica <119498316+carlosacosta-tinamica@users.noreply.github.com>
2025-07-28 13:51:22 +0200

    Update main.tf

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -1,1 +1,9 @@
-# GCP
+provider "google" {
+  project = "TU_PROJECT_ID"
+  region  = "europe-west1"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "mi-bucket-ejemplo-carlosacosta"
+  location = "EU"
+}
