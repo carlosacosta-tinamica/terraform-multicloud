91b5411f3c591bff7e8fcfdd80f5d9180da8b08e
carlosacosta-tinamica <119498316+carlosacosta-tinamica@users.noreply.github.com>
2025-07-29 08:49:51 +0200

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -7,19 +7,50 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-tf-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-tf-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-tf-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "vm-tf-demo"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
-  admin_password      = "P@ssw0rd123456!"  # Cambia esto en producción o usa secrets
-  network_interface_ids = []
+  admin_password      = "P@ssw0rd123456!" # Usa un secret real en producción
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
     name                 = "osdisk-tf-demo"
   }
+
   source_image_reference {
     publisher = "MicrosoftWindowsDesktop"
     offer     = "Windows-10"
