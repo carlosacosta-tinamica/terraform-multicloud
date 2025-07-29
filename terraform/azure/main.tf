3ea5f2409bccd800288a93bc7d89298d692c7729
carlosacosta-tinamica <119498316+carlosacosta-tinamica@users.noreply.github.com>
2025-07-29 10:49:48 +0200

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -2,7 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-# Referencia al grupo de recursos existente
 data "azurerm_resource_group" "existing" {
   name = "rg-earis-res-001"
 }
@@ -21,39 +20,80 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-tf-demo"
+# NIC para Windows
+resource "azurerm_network_interface" "nic_windows" {
+  name                = "nic-tf-win"
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
 
   ip_configuration {
-    name                          = "ipconfig1"
+    name                          = "ipconfig-win"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_windows_virtual_machine" "vm" {
-  name                = "vm-tf-demo"
+# NIC para Linux
+resource "azurerm_network_interface" "nic_linux" {
+  name                = "nic-tf-linux"
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+
+  ip_configuration {
+    name                          = "ipconfig-linux"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# VM Windows Server
+resource "azurerm_windows_virtual_machine" "vm_windows" {
+  name                = "vm-tf-win"
   resource_group_name = data.azurerm_resource_group.existing.name
   location            = data.azurerm_resource_group.existing.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
   admin_password      = "131560carlos*AZURE2024!"
   network_interface_ids = [
-    azurerm_network_interface.nic.id
+    azurerm_network_interface.nic_windows.id
   ]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    name                 = "osdisk-tf-demo"
+    name                 = "osdisk-tf-win"
   }
 
   source_image_reference {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "Windows-10"
-    sku       = "20h2-pro"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+# VM Linux (Ubuntu)
+resource "azurerm_linux_virtual_machine" "vm_linux" {
+  name                = "vm-tf-linux"
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  admin_password      = "131560carlos*AZURE2024!"  # O usa SSH para mayor seguridad
+  network_interface_ids = [
+    azurerm_network_interface.nic_linux.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "osdisk-tf-linux"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18_04-lts"
     version   = "latest"
   }
 }
